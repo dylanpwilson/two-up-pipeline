8bf0da9fa068bb46c264108b49b1a474208c6e8a
dylanpwilson <91587513+dylanpwilson@users.noreply.github.com>
2024-01-08 18:15:13 +0000

    created sql connections and insert funs

---
+++ scripts/main.py
@@ -0,0 +1,6 @@
+# test = ['date', 'name']
+# print(str(tuple(test)).replace('\'','"'))
+
+val_str = '%s, ' * 10
+val_str = val_str[:len(val_str)-2]
+print(val_str)
--- scripts/sql_functions.py
+++ scripts/sql_functions.py
@@ -1,6 +1,35 @@
 import pymysql
 
-def sql_connection(hostname, username, password, db):
+
+def sql_connection(hostname, username, passwrd, db):
     cnx = pymysql.connect(
-        host=hostname
+        host=hostname,
+        user=username,
+        password=passwrd,
+        database=db
     )
+
+    return cnx
+
+
+def sql_close_connection(connection):
+    connection.close()
+
+
+def sql_insert_data_pt(connection, table, data_pt):
+
+    keys_sorted = sorted([key for key in data_pt.keys()])
+    var_str = str(tuple(keys_sorted)).replace('\'', '"')
+    val_str = '%s, ' * len()
+    val_str = val_str[:len(val_str)-2]
+
+    sql_statement = f'INSERT INTO {table} {var_str} VALUES ({val_str});'
+    values = tuple([data_pt[key] for key in keys_sorted])
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(sql_statement, values)
+
+    connection.commit()
+
+
+
